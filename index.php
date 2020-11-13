be8a8620d146f9d4f7b2391a25a3b6f60a58117d
Kinga Wrona <wronakinga98@gmail.com>
2020-11-13 19:32:03 +0100

    Update index.php
    update

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], "/");
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('', 'DefaultController');
+Routing::get('login', 'DefaultController');
+Routing::get('registration', 'DefaultController');
+Routing::get('search', 'DefaultController');
+Routing::get('advertisement', 'DefaultController');
+Routing::get('firstForm', 'DefaultController');
+Routing::get('secondForm', 'DefaultController');
+Routing::get('thirdForm', 'DefaultController');
+Routing::get('fourthForm', 'DefaultController');
+Routing::get('fifthForm', 'DefaultController');
+
+Routing::run($path);
