438382cb3697bc32fb89247b1072122124eb4cf4
Kinga Wrona <wronakinga98@gmail.com>
2020-11-27 20:16:02 +0100

    Create home-page.php
    rename a file

---
+++ public/views/home-page.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="public/css/style.css">
+        <link rel="stylesheet" type="text/css" href="public/css/style-menu.css">
+        <link rel="stylesheet" type="text/css" href="public/css/style-advertisements-section.css">
+        <link rel="stylesheet" type="text/css" href="public/css/style-home-page.css">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home Page</title>
+    </head>
+    <body>
+        <div class="base-container">
+            <header>
+                <div class="logo">
+                    <img src="public/img/logo.svg">
+                </div>
+                <nav>
+                    <button>
+                        <i class="fas fa-bars"></i>
+                    </button>
+                    <ul>
+                        <li><a href="#">MOJE KONTO</a></li>
+                        <li><a href="#">DODAJ OGŁOSZENIE</a></li>
+                        <li><a href="#">PRZEGLĄDAJ OGŁOSZENIA</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <main>
+                <form class="search-panel">
+                    <input type="text" placeholder="Lokalizacja">
+                    <select>
+                        <option selected disabled>Rodzaj nieruchomości</option>
+                        <option>Mieszkanie</option>
+                        <option>Pokój</option>
+                    </select>
+                    <input type="number" step="any" placeholder="Cena od">
+                    <input type="number" step="any" placeholder="Cena do">
+                    <input type="number" step="any" placeholder="Powierzchnia od">
+                    <input type="number" step="any" placeholder="Powierzchnia do">
+                    <button id="button-search">Wyszukaj</button>
+                </form>
+                <section class="advertisements">
+                    <div id="title-section">
+                        NOWE OGŁOSZENIA
+                    </div>
+                    <section>
+                        <div class="advertisement">
+                            <img src="public/img/room.jpg">
+                            <div class="description">
+                                Lokalizacja<br/>
+                                Powierzchnia<br/>
+                                Cena<br/>
+                            </div>
+                        </div>
+                        <div class="advertisement">
+                            <img src="public/img/room.jpg">
+                            <div class="description">
+                                Lokalizacja<br/>
+                                Powierzchnia<br/>
+                                Cena<br/>
+                            </div>
+                        </div>
+                        <div class="advertisement">
+                            <img src="public/img/room.jpg">
+                            <div class="description">
+                                Lokalizacja<br/>
+                                Powierzchnia<br/>
+                                Cena<br/>
+                            </div>
+                        </div>
+                        <div class="advertisement">
+                            <img src="public/img/room.jpg">
+                            <div class="description">
+                                Lokalizacja<br/>
+                                Powierzchnia<br/>
+                                Cena<br/>
+                            </div>
+                        </div>
+                        <div class="advertisement">
+                            <img src="public/img/room.jpg">
+                            <div class="description">
+                                Lokalizacja<br/>
+                                Powierzchnia<br/>
+                                Cena<br/>
+                            </div>
+                        </div>
+                        <div class="advertisement">
+                            <img src="public/img/room.jpg">
+                            <div class="description">
+                                Lokalizacja<br/>
+                                Powierzchnia<br/>
+                                Cena<br/>
+                            </div>
+                        </div>
+                    </section>
+                </section>
+            </main>
+        </div>
+    </body>
+</html>
