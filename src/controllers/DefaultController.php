41947b3f90d3062cac393e0a59509859618d582c
Kinga Wrona <wronakinga98@gmail.com>
2020-12-05 21:56:54 +0100

    Update DefaultController.php
    removing some redundant functions

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -23,24 +23,4 @@ class DefaultController extends AppController {
     public function advertisement() {
         $this->render('advertisement-page');
     }
-
-    public function firstForm() {
-        $this->render('first-page-add-advertisement-form');
-    }
-
-    public function secondForm() {
-        $this->render('second-page-add-advertisement-form');
-    }
-
-    public function thirdForm() {
-        $this->render('third-page-add-advertisement-form');
-    }
-
-    public function fourthForm() {
-        $this->render('fourth-page-add-advertisement-form');
-    }
-
-    public function fifthForm() {
-        $this->render('fifth-page-add-advertisement-form');
-    }
 }
